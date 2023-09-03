1f2e1bb1d22e6276597952894317e918b1670987
AmirKafi <doostkafiamir@gmail.com>
2023-09-03 19:41:46 +0430

    User profile

---
+++ FallLady.Mood/Controllers/UserProfileController.cs
@@ -0,0 +1,13 @@
+﻿using FallLady.Mood.Controllers.Base;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FallLady.Mood.Controllers
+{
+    public class UserProfileController : BaseController
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
